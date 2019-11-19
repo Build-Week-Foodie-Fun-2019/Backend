45473996fd5aa59098486660c2b20ed5e724eec6
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-19 10:06:16 +0100

    server listener and routes separated

---
+++ api/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+const server = express();
+server.use(helmet());
+
+const foodies = [
+    {id:1, name:'Ifiok'},
+    {id:2, name:'Kayla'},
+    {id:3, name:'Blake'},
+];
+
+server.use(express.json());
+server.use(cors());
+
+
+server.get('/', (req, res) => {
+    res.send("<h1>Welcome to Foodie Fun!</h1>")
+   })
+   server.get('/api/foodies', (req, res, next) => {
+     res.json(foodies);
+   });
+
+   module.exports = server;
