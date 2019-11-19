8c6a5d0624e30e0db9b1f351d19cb0d3b1bdfb1e
Oluwajoba <oluwajobabello@gmail.com>
2019-11-19 11:52:38 +0100

    add endpoints for post(/register) / auth-router.js

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -3,3 +3,24 @@ const router = require('express').Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const Users = require('../users/userModel');
+
+router.post('/register', (req, res) => {
+    // implement registration
+    let user = req.body;
+      const hash = bcrypt.hashSync(user.password, 10);
+      user.password = hash;
+  
+      Users.add(user)
+          .then(saved => {
+              res.status(201).json(saved);
+          })
+          .catch(error => {
+              res.status(500).json(error);
+          });
+  
+  });
+
+
+
+  module.exports = router;
