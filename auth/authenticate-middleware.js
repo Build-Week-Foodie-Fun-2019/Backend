98b5b4579d2e658921436b77667b1b96a9c1d261
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-19 15:10:23 +0100

    removed secret from plain sight

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -1,24 +1,24 @@
-
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 module.exports = (req, res, next) => {
- 
   const token = req.headers.authorization;
 
-    if (token) {
-        jwt.verify(
-            token,
-            'THIS IS THE SECRET',
-            (err, decodedToken) => {
-                if (err) {
-                    res.status(401).json({ message: 'shall not pass!' + err.message })
-                } else {
-                    req.decodedToken = decodedToken;
-                    next()
-                }
-            }
-        )
-    } else {
-        res.status(400).json({ message: 'No credentials provided' });
-    }
+  if (token) {
+    jwt.verify(
+      token,
+      process.env.NODE_ENV === "development"
+        ? process.env.SECRET
+        : process.env.SECRET,
+      (err, decodedToken) => {
+        if (err) {
+          res.status(401).json({ message: "shall not pass!" + err.message });
+        } else {
+          req.decodedToken = decodedToken;
+          next();
+        }
+      }
+    );
+  } else {
+    res.status(400).json({ message: "No credentials provided" });
+  }
 };
