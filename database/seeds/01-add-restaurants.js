a1abadbb0bb47c49d51eb5d19d6fc24ef53d8a47
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-20 10:41:41 +0100

    added seed files to populate db

---
+++ database/seeds/01-add-restaurants.js
@@ -0,0 +1,37 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("restaurants")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("restaurants").insert([
+        {
+          restaurant_id: 1,
+          restaurant_name: "Pepperoni",
+          restaurant_cuisine: "Mexican",
+          restaurant_location: "Richmond, VA",
+          restaurant_hours: "M - Sa 11a - 9p, Su 12p - 7p",
+          restaurant_rating: 4,
+          restaurant_image: ""
+        },
+        {
+          restaurant_id: 2,
+          restaurant_name: "Cafe Americano",
+          restaurant_cuisine: "Bistro",
+          restaurant_location: "St. Louis, Missouri",
+          restaurant_hours: "M - Su - 7a - 10p",
+          restaurant_rating: 3,
+          restaurant_image: ""
+        },
+        {
+          restaurant_id: 3,
+          restaurant_name: "Katz's Deli",
+          restaurant_cuisine: "Deli",
+          restaurant_location: "Manhattan, NY",
+          restaurant_hours: "M - Su 8am - 11pm",
+          restaurant_rating: 5,
+          restaurant_image: ""
+        }
+      ]);
+    });
+};
---
+++ database/seeds/04-add-user-restaurants.js
@@ -0,0 +1,31 @@
+exports.seed = function(knex) {
+  // Deletes ALL existing entries
+  return knex("user_restaurants")
+    .truncate()
+    .then(function() {
+      // Inserts seed entries
+      return knex("user_restaurants").insert([
+        {
+          user_restaurant_id: 1,
+          user_restaurant_user_id: 1,
+          user_restaurant_restaurant_id: 2,
+          user_restaurant_review:
+            "Great but pricey sandwiches. Worth the long wait."
+        },
+        {
+          user_restaurant_id: 2,
+          user_restaurant_user_id: 1,
+          user_restaurant_restaurant_id: 1,
+          user_restaurant_review:
+            "Great food and friendly staff! I’ll be back!"
+        },
+        {
+          user_restaurant_id: 3,
+          user_restaurant_user_id: 2,
+          user_restaurant_restaurant_id: 1,
+          user_restaurant_review:
+            "Okay coffee and better than average breakfast sandwiches."
+        },
+      ]);
+    });
+};
