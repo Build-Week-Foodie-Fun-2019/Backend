9696fc5bd3857e7e2a65c31316f2b00700ce2962
Oluwajoba <oluwajobabello@gmail.com>
2019-11-20 12:49:50 +0100

    create and update menuItems/menuItemsRouuter.js / add get(/)

---
+++ menuItems/menuItemRouter.js
@@ -0,0 +1,15 @@
+
+const router = require('express').Router();
+
+const dB = require('./menuItemModel');
+
+router.get('/', (req, res) => {
+
+    dB.getAll()
+        .then((menu_items) => {
+            res.status(200).json(menu_items)
+        })
+        .catch(() => {
+            res.status(500).json({ error: "The menu_items information could not be retrieved." })
+        })
+});
