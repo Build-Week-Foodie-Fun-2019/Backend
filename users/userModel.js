08316eb8150a2667ec734aa330c6603ad0593e9c
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-20 20:56:07 +0100

    wip postgres

--- users/userModel.js
+++ users/userModel.js
@@ -10,13 +10,14 @@ module.exports = {
 async function add(user) {
   const [user_id] = await db("users")
     .insert(user, "user_id")
-    .returning([
-      "user_id",
-      "user_username",
-      "user_password",
-      "user_email",
-      "user_location"
-    ]);
+    // .returning([
+    //   "user_id",
+    //   "user_username",
+    //   "user_password",
+    //   "user_email",
+    //   "user_location"
+    // ]);
+    console.log(user_id)
 
     const newUser = await findById(user_id)
     return newUser;
