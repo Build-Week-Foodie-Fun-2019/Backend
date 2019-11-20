d646e5eb16acf86c970c4ee1f9ad05866fd013e7
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-20 21:00:43 +0100

    edited seed

--- database/seeds/00-add-users.js
+++ database/seeds/00-add-users.js
@@ -6,21 +6,21 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex("users").insert([
         {
-          user_id: 1,
+         
           user_username: "josh_armantrout",
           user_password: "p4$$w0rd",
           user_email: "josh.armantrout@email.com",
           user_location: "Richmond, Virginia, US"
         },
         {
-          user_id: 2,
+         
           user_username: "iffy_oak",
           user_password: "c00l_pa$$word",
           user_email: "ifiok@email.com",
           user_location: "Calabar, Cross River State, NG"
         },
         {
-          user_id: 3,
+       
           user_username: "oluwajoba_bello",
           user_password: "be11o_p4$$w0rd",
           user_email: "oluwajoba@email.com",
