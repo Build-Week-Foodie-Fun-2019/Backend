05a260137f6d4845eb775ff486992db2e679226e
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-20 13:30:36 +0100

    added postgres syntax for the restaurant db model

--- restaurants/restModel.js
+++ restaurants/restModel.js
@@ -1,25 +1,34 @@
 const db = require("../database/db-config");
 
 module.exports = {
-    getAll,
-    getById,
-    insert,
+  getAll,
+  getById,
+  insert
 };
 
 function getAll() {
-    return db('restaurants');
+  return db("restaurants");
 }
 
 function getById(restaurant_id) {
-    return db('restaurants')
-        .where({ restaurant_id })
-        .first();
+  return db("restaurants")
+    .where({ restaurant_id })
+    .first();
 }
 
 function insert(restaurant) {
-    return db('restaurants')
-        .insert(restaurant)
-        .then(ids => {
-            return getById(ids[0]);
-        });
+  return db("restaurants")
+    .insert(restaurant)
+    .returning([
+      "restaurant_id",
+      "restaurant_name",
+      "restaurant_cuisine",
+      "restaurant_location",
+      "restaurant_hours",
+      "restaurant_rating",
+      "restaurant_image"
+    ])
+    .then(ids => {
+      return getById(ids[0]);
+    });
 }
