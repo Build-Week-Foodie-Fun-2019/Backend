fbc7c4ce55d5465869e3b9aec76b08debaad5ecb
ifiokudoidiok <ifiok_udoidiok@yahoo.com>
2019-11-20 19:43:00 +0100

    edited seed commands

--- database/seeds/03-add-menu-items.js
+++ database/seeds/03-add-menu-items.js
@@ -1,7 +1,7 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
   return knex("menu_items")
-    .truncate()
+    .del()
     .then(function() {
       // Inserts seed entries
       return knex("menu_items").insert([
