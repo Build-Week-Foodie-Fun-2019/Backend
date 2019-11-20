654d40d52eb5187d4e7aaa0a235734139f24118e
Oluwajoba <oluwajobabello@gmail.com>
2019-11-20 13:01:16 +0100

    update menuItems/menuItemsRouuter.js with middleware

--- menuItems/menuItemRouter.js
+++ menuItems/menuItemRouter.js
@@ -17,9 +17,28 @@ router.get('/', (req, res) => {
 
 router.get('/:id', validateUserMenu_itemId, (req, res) => {
 
-    res.status(200).json({ reviewByUserMenu_itemId: req.menu_item })
+    res.status(200).json({ menu_itemByUserMenu_itemId: req.menu_item })
   });
 
+
+
+  function validateUserMenu_itemId(req, res, next) {
+
+    const { id } = req.params;
+    dB.getById(id)
+      .then((menu_item) => {
+        if (menu_item) {
+          req.menu_item = menu_item;
+          next();
+        }
+        else {
+          res.status(404).json({ message: "The menu_item with the specified ID does not exist." })
+        }
+      })
+      .catch((err) => {
+        res.status(500).json({ error: "The menu_item information could not be retrieved." + err })
+      })
+  }
 
 
 module.exports = router
