5fe435bf54aeb78e1cfe21c2d1a6bf7ef20b88c7
Oluwajoba <oluwajobabello@gmail.com>
2019-11-21 09:35:03 +0100

    update userModel.js with getAllRestaurant()

--- users/userModel.js
+++ users/userModel.js
@@ -1,36 +1,39 @@
+
 const db = require("../database/db-config");
 
 module.exports = {
   add,
   findBy,
   findById,
-  getAllUsers
+  getAllUsers,
+  getAllRestaurants
+
 };
 
 async function add(user) {
   const [user_id] = await db("users")
     .insert(user, "user_id")
-    // .returning([
-    //   "user_id",
-    //   "user_username",
-    //   "user_password",
-    //   "user_email",
-    //   "user_location"
-    // ]);
-    console.log(user_id)
+  // .returning([
+  //   "user_id",
+  //   "user_username",
+  //   "user_password",
+  //   "user_email",
+  //   "user_location"
+  // ]);
+  console.log(user_id)
 
-    const newUser = await findById(user_id)
-    return newUser;
+  const newUser = await findById(user_id)
+  return newUser;
 }
 
 function findById(user_id) {
   return db("users")
-  .select(
-    "user_id",
-    "user_username",
-    "user_email",
-    "user_location"
-  )
+    .select(
+      "user_id",
+      "user_username",
+      "user_email",
+      "user_location"
+    )
     .where({ user_id })
     .first();
 }
@@ -50,3 +53,9 @@ function getAllUsers() {
   );
 }
 
+function getAllRestaurants(id) {
+  return db.select("restaurant_name")
+    .from("restaurants")
+    .join("users")
+    .where("users.users_id", "=", id)
+}
